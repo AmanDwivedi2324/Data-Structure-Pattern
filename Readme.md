c8de8e11575d58cd08e53f14bdf132bc7a3c0c90
AmanDwivedi2324 <0786ad2000@gmail.com>
2026-06-30 22:02:42 +0530

    Remove duplicates from sorted array

--- Readme.md
+++ Readme.md
@@ -1,3 +1,6 @@
+# Two pointer 
 Two_Sum_II_Input_Array_Is_Sorted = https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/submissions/2050217136/
 <br>
 Segregate 0s and 1s = https://www.geeksforgeeks.org/problems/segregate-0s-and-1s5106/1
+<br>
+Remove duplicates from sorted array = https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/2051436272/
---
+++ Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public static int removeDuplicates(int[] nums) {
+        // int j=1;
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i]!=nums[i-1]){
+        //         nums[j] = nums[i];/
+        //         j++;
+        //     }
+        // }
+        // return j;
+
+
+
+
+        int n = nums.length;
+        int i=0;
+        int j=i+1;
+
+        while(j < n){
+           if(nums[i] == nums[j]){
+            j++;
+           }
+           else{
+            i++;
+            nums[i] = nums[j];
+           }
+        }
+
+        return i+1;
+
+
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {0,0,1,1,1,2,2,3,3,4};
+        int ans = removeDuplicates(arr);
+        System.out.print(ans);
+    }
+}
